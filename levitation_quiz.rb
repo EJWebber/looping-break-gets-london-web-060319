e0cd20a10d6bdb383cd3f4ef4064a56deb2875af
Ed Webber <edwebber06@gmail.com>
2019-05-19 11:48:51 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,7 +3,7 @@ def levitation_quiz
 	loop do
 	  puts "What is the spell for levitation?"
 	  answer = gets.chomp
-	  break is answer == "Wingardium Leviosa"
+	  break if answer == "Wingardium Leviosa"
 	end
 	puts "You passed the quiz!"
 end
